3e356f5505e5dff6df898158ce3530612df697a0
Dodis <dodis@pop-os.localdomain>
2022-09-14 14:14:35 +0300

    new

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,15 @@ const { response } = require('express');
 
 express = require('express');
 const router = express.Router();
-
+const notes = [
+    {text: "Köp Mat"},
+    {text: "Yo mama"}
+]
 router.get('/', (req, res) => {
-    response.send('notes!');
+    response.send(notes);
 })
-
+router.post('/', (req, res) => {
+    notes.push(req.body);
+    res.send(req.body)
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3030
-const fooRouter = require('./routes/foo')
+const mongoose = require('mongoose')
 
+require('dotenv').config()
+mongoose.connect(process.env.DB_URL)
+
+console.log(process.env.DB_URL)
 app.get('/', (req, res) => {
-    res.send(`Express says hello ${res.locals.myVariables}`)
+    res.send({msg: 'hello'})
 })
+
 const notesRouter = require('./routes/notes')
 app.use('/notes', notesRouter);
 console.log("Hello world");
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
